0ea4ef90f2640303299e571fc35786ce6cd9e71c
Hiccham <hicham.hachem.2@isae.edu.lb>
2021-12-17 17:54:36 +0200

    Update AuditeurCNAM.java

--- tp1/question3/AuditeurCNAM.java
+++ tp1/question3/AuditeurCNAM.java
@@ -1,90 +1,69 @@
 package question3;
 
-/**
- * NFP121 TpIntroduction, usage de BlueJ et du "Submitter".
- * 
- * @version septembre 2009
- * @author à compléter
- * @see java.lang.String, java.lang.Math
- */
-public class AuditeurCNAM {
-    /** l'attribut nom de chaque auditeur. */
+import java.text.Normalizer;
+
+public class AuditeurCNAM{
+    
     private String nom;
-    /** l'attribut prenom de chaque auditeur. */
     private String prenom;
-    /** l'attribut matricule de chaque auditeur. */
     private String matricule;
-
-    /**
-     * "Création", le constructeur d'un auditeur avec son nom, son prénom et son
-     * matricule.
-     * 
-     * @param nom
-     *            le nom de l'auditeur
-     * @param prenom
-     *            son prénom
-     * @param matricule
-     *            sur la carte d'inscription, près de la photo
-     */
-    public AuditeurCNAM(String nom, String prenom, String matricule) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.matricule = matricule;
+    
+    public void setNom(String _nom){
+        this.nom = _nom;
+    }
+    
+    public void setPrenom(String _prenom){
+        this.prenom = _prenom;
+    }
+        
+    public void setMatricule(String _matricule){
+        this.matricule = _matricule;
+    }
+    
+    public String getNom() {
+        return this.prenom;
+    }
+        
+    public String getPrenom() {
+        return this.prenom;
+    }
+    
+    public String getMatricule() {
+        return this.matricule;
     }
 
-    /**
-     * le login au Cnam : 6 premières lettres du nom suivies de la première
-     * lettre du prénom séparées de '_' . le login retourné est en minuscules,
-     * le trait d'union, ou spéciaux <i>(pour unix)</i> sont remplacés par des
-     * '_' pas de caractères accentués pour le login voir les classes
-     * prédéfines, java.lang.String : les méthodes replaceAll, toLowerCase et
-     * substring java.lang.Math : la méthode min<br>
-     * <b>BlueJ : Menu Aide</b>
-     * 
-     * @return le login du Cnam simplifié, sans les adaptations dues aux
-     *         homonymes...
-     */
+    public AuditeurCNAM() {
+        this.nom = "testnom";
+        this.prenom = "testprenom";
+        this.matricule = "testmatricule";
+    }
+    
+    public AuditeurCNAM(String _nom, String _prenom, String _matricule) {
+        this.nom = _nom;
+        this.prenom = _prenom;
+        this.matricule = _matricule;
+    }
+    
+    public static String stripAccents(String s) {
+        s = Normalizer.normalize(s, Normalizer.Form.NFD);
+        s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+        return s;
+    }
+
     public String login() {
-        return "";// à compléter
+        String n;
+        String p;
+        n=this.nom.substring(0,Math.min(this.nom.length(),6));
+        p=this.prenom.substring(0,1);
+        n=n.toLowerCase();
+        p=p.toLowerCase();
+        n=stripAccents(n);
+        p=stripAccents(p);
+        return n+"_"+p;
     }
 
-    /**
-     * Lecture du nom de l'auditeur.
-     * 
-     * @return son nom
-     */
-    public String nom() {
-        return null;// à compléter
-    }
-
-    /**
-     * Lecture du prénom de l'auditeur.
-     * 
-     * @return son prénom
-     */
-    public String prenom() {
-        return null;// à compléter
-    }
-
-    /**
-     * Lecture du matricule de l'auditeur.
-     * 
-     * @return son matricule
-     */
-    public String matricule() {
-        return null;// à compléter
-    }
-
-    /**
-     * méthode toString ( méthode redéfinie de la classe Object).
-     * 
-     * @return la concaténation du nom, du prénom et du login, selon cette
-     *         syntaxe
-     *         <code>nom() + " " + prenom() +  " login : " + login()</code>
-     */
     @Override
     public String toString() {
-        return nom() + " " + prenom() + " login : " + login();
+        return getNom() + " " + getPrenom() + " login : " + login();
     }
-
 }
